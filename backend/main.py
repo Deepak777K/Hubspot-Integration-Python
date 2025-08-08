e65168796eb970d6181746b79b39e47b2a295bc8
Deepakwasiya <deepak.wasiya1@gmail.com>
2025-08-08 17:43:51 +0530

    feat(Hubspot-Integration) Add OAuth 2.0 authorization flow for connecting to HubSpot

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 # Server file
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from urllib.parse import quote
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello from FastAPI!"}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request
+    })
+
+CLIENT_ID = 'CLIENT ID'
+SCOPES = 'SCOPES'
+REDIRECT_URI = 'REDIRECT_URI'
+STATE = 'STATE'
+
+@app.get("/integration/connect")
+async def authorize_hubspot():
+    authUrl = f'https://app.hubspot.com/oauth/authorize?client_id={quote(CLIENT_ID)}&scope={quote(SCOPES)}&redirect_uri=${quote(REDIRECT_URI)}&state={quote(STATE)}'
+    return RedirectResponse(url=authUrl)
